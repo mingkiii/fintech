11eb2d9a56f605cb24c8e551d17e8be77aa06ea3
ming <123580123+mingkiii@users.noreply.github.com>
2023-05-31 10:45:35 +0900

    file commit

--- api/src/main/kotlin/com/zerobase/api/ApiApplication.kt
+++ api/src/main/kotlin/com/zerobase/api/ApiApplication.kt
@@ -6,9 +6,6 @@ import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.EnableAspectJAutoProxy
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.SQLException
 
 @SpringBootApplication
 @EntityScan(basePackages = ["com.zerobase.domain"])
@@ -20,16 +17,3 @@ class ApiApplication
 fun main(args: Array<String>) {
     runApplication<ApiApplication>(*args)
 }
-//fun main() {
-//    val url = "jdbc:mysql://fintech-mysql:3306/fintech?characterEncoding=UTF-8&serverTimezone=Asia/Seoul"
-//    val username = "fintech"
-//    val password = "fintech"
-//
-//    try {
-//        val connection: Connection = DriverManager.getConnection(url, username, password)
-//        println("접속 성공!")
-//        connection.close()
-//    } catch (e: SQLException) {
-//        println("접속 실패: ${e.message}")
-//    }
-//}
